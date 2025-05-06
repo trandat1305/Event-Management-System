c212697fce1c2f0beec70958b1448b7803cacd24
DangKhoi143 <s4015143@rmit.edu.vn>
2025-05-06 11:13:04 +0700

    finally can connect to mongodb

--- Event-Management/server/server.js
+++ Event-Management/server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const path = require('path');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const PORT = process.env.PORT || 3000;
 

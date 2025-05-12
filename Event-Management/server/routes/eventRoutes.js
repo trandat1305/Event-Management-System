e40d5b57e4e5b5416262281c54665d3683210d67
DangKhoi143 <s4015143@rmit.edu.vn>
2025-05-13 00:32:10 +0700

    finalized event router?

--- Event-Management/server/routes/eventRoutes.js
+++ Event-Management/server/routes/eventRoutes.js
@@ -2,21 +2,31 @@ const express = require('express');
 const eventRouter = express.Router();
 const eventController = require('../controllers/eventController');
 
-const upload = require('../configuration/multer'); // Import Multer
+const upload = require('../middlewares/uploadImages'); // Import Multer
 
-// Protected routes (organizer/admins only)
 eventRouter.use(authMiddleware); // Protect all routes below this line
 
-eventRouter.post('/createEvent', upload.single('image'), createEvent); // Use Multer for image upload
+eventRouter.post('/createEvent', upload.single('image'), eventController.createEvent);
 
-eventRouter.put('updateEvent/:id', authMiddleware, updateEvent);
-eventRouter.delete('/:id', authMiddleware, deleteEvent);
+eventRouter.put('updateEvent/:id', eventController.updateEvent);
 
-// Public route
-eventRouter.get('/publicEvents', getPublicEvents);
+eventRouter.put('/updateEvent/:id', upload.single('image'), eventController.updateEventImage); // update an event
 
-//calendar route
-eventRouter.get('/calendar', authenMiddleware, getCalendarEvents);
+eventRouter.delete('/:id', eventController.deleteEvent); 
 
-module.exports = router;
+eventRouter.get('/getEvent/:id', eventController.getEventById); // get an event by ID
+
+eventRouter.get('/getAllEvents', eventController.getAllEvents); // get all events that a person is attending
+
+eventRouter.get('/getAllEvents', eventController.getAllPublicEvents); // get all events that a person is attending
+
+eventRouter.get('/privateEvents', eventController.getPrivateEvents); // get all private events that a person is attending
+
+eventRouter.get('/publicEvents', eventController.getPublicEvents); // get all public events that a person is attending
+
+eventRouter.get('/getOrganizedEvents', eventController.getOrganizedEvents); // get all events that a person is organizing
+
+eventRouter.get('/getMyevents', eventController.getMyEvents); // get all events that a person has made
+
+module.exports = eventRouter;
 

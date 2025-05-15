2a3491564f864e2fd6e74a2987981b428f88839b
DangKhoi143 <s4015143@rmit.edu.vn>
2025-05-15 11:03:06 +0700

    more changes to routes (this is never gonna end)

--- Event-Management/server/routes/invitationRoutes.js
+++ Event-Management/server/routes/invitationRoutes.js
@@ -11,12 +11,8 @@ invitationRouter.post('/:inviteId/accept', invitationController.acceptInvitation
 
 invitationRouter.post('/:inviteId/deny', invitationController.rejectInvitation); // reject an invitation
 
-invitationRouter.get('/all', invitationController.getAllInvitations); // get all invitations for a user
-
-invitationRouter.get('/all/:eventId', invitationController.getAllInvitationsForEvent); // get all invitations for an event
+invitationRouter.get('/', invitationController.getAllInvitations); // get all invitations for a user
 
 invitationRouter.get('/sent', invitationController.getSentInvitations); // get all sent invitations
 
-invitationRouter.get('/sent/:eventId', invitationController.getSentInvitationsForEvent); // get all sent invitations for an event
-
-module.exports = router;
+module.exports = invitationRouter;

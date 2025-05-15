d758529cb4dd12ef8262840ea0e940fb38d22464
DangKhoi143 <s4015143@rmit.edu.vn>
2025-05-15 11:03:42 +0700

    finished notification controller, just need implementations at everywhere else

--- Event-Management/server/controllers/notificationController.js
+++ Event-Management/server/controllers/notificationController.js
@@ -1,7 +1,7 @@
 const Notification = require('../models/Notification');
 
 // Get notifications with pagination
-exports.getNotifs = async (req, res) => {
+exports.getNotification = async (req, res) => {
   try {
     const userId = req.user._id;
 
@@ -13,5 +13,50 @@ exports.getNotifs = async (req, res) => {
     res.status(200).json(notifications);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch notifications', errorMessage: err.message });
+  }
+};
+
+exports.createNotification = async ({ userId, eventId, message, type }) => {
+  try {
+    const notification = new Notification({
+      userId,
+      eventId,
+      message,
+      type,
+    });
+
+    await notification.save();
+    return notification;
+  } catch (error) {
+    console.error('Error creating notification:', error);
+    throw error;
+  }
+};
+
+exports.deleteNotification = async (req, res) => {
+  try {
+    const notificationId = req.params.notificationId;
+
+    const notification = await Notification.findById(notificationId);
+
+    if (!notification) {
+      return res.status(404).json({ error: 'Notification not found' });
+    }
+
+    await notification.softDelete();
+    res.status(200).json({ message: 'Notification deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete notification', errorMessage: err.message });
+  }
+}
+
+exports.deleteAllNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ userId: userId });
+    res.status(200).json({ message: 'All notifications deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete notifications', errorMessage: err.message });
   }
 };
